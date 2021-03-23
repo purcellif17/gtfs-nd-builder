af348d6fae9f46339b52eac3d1434c262e361905
purcellif17 <ianp122@gmail.com>
2021-03-23 19:58:14 -0400

    Added code to create nd and added doc updates

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Builds a geospatial network dataset from current MBTA GTFS data.
 
 At the moment, this script downloads the GTFS data and runs it through the ArcGIS tool to create a usable network. Future improvements include downloading street data, and actuallyg generating a network dataset.
+
+This code is based on the workflow of the [Create and use a network dataset with public transit data](https://pro.arcgis.com/en/pro-app/latest/help/analysis/networks/create-and-use-a-network-dataset-with-public-transit-data.htm) tutorial for ArcGIS Pro. The TransitnetworkTemplate.xml document comes straight from that tutorial and I take no credit for it.
--- gtfs_retriever.py
+++ gtfs_retriever.py
@@ -1,6 +1,7 @@
 # pylint: disable=E1101
 
 import os
+import arcpy
 from datetime import datetime
 
 date = datetime.today().strftime('%Y%m%d')
@@ -25,15 +26,14 @@ def gtfs_retriever(gtfs_source):
     return raw_path
 
 def gtfs_cleaner(gtfs_folder):
+    
     import pandas as pd
 
     df = pd.read_csv(gtfs_folder+'\\stops.txt')
     df = df.dropna(subset=['stop_lat', 'stop_lon'])
     df.to_csv(gtfs_folder+'\\stops.txt',index=False)
 
-
-def nd_builder(gtfs_folder):
-    import arcpy
+def node_generator(gtfs_folder):
 
     print('Building file geodatabase')
     gdb_folder = os.path.join(os.path.expanduser('~'),"OneDrive","Documents","ArcGIS","Projects","MBTA_Overview","GTFS","GTFS_gdb")
@@ -42,18 +42,40 @@ def nd_builder(gtfs_folder):
         arcpy.Delete_management(gdb_path)
     gdb = arcpy.management.CreateFileGDB(gdb_folder, f"MBTA_GTFS_{date}")
     fd = arcpy.CreateFeatureDataset_management(gdb,"transit_network",'102100')
+    path = arcpy.Describe(fd).catalogPath
 
-    print('Building network')
+    print('Building network nodes')
     arcpy.conversion.GTFSToNetworkDatasetTransitSources(gtfs_folder,fd,'INTERPOLATE')
+
+    return path
+
+def build_nd(template,gtfs_fd):
+    nd = arcpy.na.CreateNetworkDatasetFromTemplate(template,gtfs_fd)
+
+    # TODO: get network to build itself.
+    #network = arcpy.na.BuildNetwork(nd)
+    network = "This is a placeholder"
+    return network
 
 def main():
 
+    # Extract GTFS data and save to a folder.
     gtfs_source = 'https://cdn.mbta.com/MBTA_GTFS.zip'
     gtfs_folder = gtfs_retriever(gtfs_source)
-
+    # Clean GTFS data for consumption in GIS.
     gtfs_cleaner(gtfs_folder)
+    # Build nodes and paths from GTFS data. Returns feature dataset with datapoints for network.
+    gtfs_fd = node_generator(gtfs_folder)
 
-    nd_builder(gtfs_folder)
+    # TODO: Add code to pull and prep streets from MassDOT Road Inventory. This section will be converted to a module.
+    streets_source = os.path.join(os.path.expanduser('~'),"OneDrive","Documents","ArcGIS","Projects","MBTA_Overview","MBTA_Overview.gdb","streets")
+    streets = arcpy.conversion.FeatureClassToFeatureClass(streets_source,gtfs_fd,"streets")
+
+    # Connect to streets. Some warnings are expected. Will handle these in the future, but usually requires some manual cleanup.
+    arcpy.conversion.ConnectNetworkDatasetTransitSourcesToStreets(gtfs_fd, streets, "100 Meters", "RestrictPedestrians <> 'Y'")
+
+    template = os.path.join(os.path.expanduser('~'),"OneDrive","Documents","ArcGIS","Projects","MBTA_Overview","GTFS","TransitNetworkTemplate.xml")
+    network = build_nd(template,gtfs_fd)
 
     print("Complete")
 
